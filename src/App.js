b0da57b67230d82588d980f99540fd3e60d48c93
Talha <mtalhamanzoor786@gmail.com>
2024-02-25 20:07:33 +0500

    Added News items and made a formal UI, now aimihng to fetch data  from API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 
 import React, { Component } from 'react'
 import News from './components/News';
+import NavBar from './components/NavBar';
 
 export default class App extends Component {
   render() {
     return (
       <>
+        <NavBar />
         <News />
+
       </>
     )
   }
--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 export class News extends Component {
     render() {
         return (
-            <div>
-                This news is from <strong><code>News.js</code></strong>
+            <div className='container'>
+                <div className="row my-3 d-flex justify-content-between">
+
+                    <div className="col-md-3">
+                        <NewsItem title="Hello world" description="This is a demo description" />
+                    </div>
+
+                    <div className="col-md-3">
+                        <NewsItem title="Hello world" description="This is a demo description" />
+                    </div>
+
+                    <div className="col-md-3">
+                        <NewsItem title="Hello world" description="This is a demo description" />
+                    </div>
+
+                </div>
+
+                <div className="row my-3 d-flex justify-content-between">
+
+                    <div className="col-md-3">
+                        <NewsItem title="Hello world" description="This is a demo description" />
+                    </div>
+
+                    <div className="col-md-3">
+                        <NewsItem title="Hello world" description="This is a demo description" />
+                    </div>
+
+                    <div className="col-md-3">
+                        <NewsItem title="Hello world" description="This is a demo description" />
+                    </div>
+
+                </div>
             </div>
         )
     }
 }
-
 export default News
