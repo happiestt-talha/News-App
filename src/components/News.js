aca7599f07d9cafe3fe8cd8077cdb521548d44ee
Talha <mtalhamanzoor786@gmail.com>
2024-02-25 17:00:36 +0500

    pehla commit

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class News extends Component {
+    render() {
+        return (
+            <div>
+                This news is from <strong><code>News.js</code></strong>
+            </div>
+        )
+    }
+}
+
+export default News
