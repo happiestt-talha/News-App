294e5dfa7291f66d79b7412e55ae14e05b5921a7
Talha <mtalhamanzoor786@gmail.com>
2024-03-01 00:45:03 +0500

    initial try to add Newses

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description } = this.props;
+
+        let { title, description, url, urlToImage } = this.props;
         return (
             <div>
-                <div class="card" style={{ width: "18rem;" }}>
-                    <img src="https://c.biztoc.com/p/6bb97114dda44ede/s.webp" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">{title}</h5>
-                        <p class="card-text">{description}</p>
-                        <a href="/" class="btn btn-primary">Go somewhere</a>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={urlToImage} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href={url} className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
