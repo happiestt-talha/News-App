0f00799afc3908347eaf98a8dabc12a7ce423161
Talha <mtalhamanzoor786@gmail.com>
2024-03-22 03:48:42 -0700

    added some  pro logic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 export class NavBar extends Component {
     render() {
+        const navItem=['general','business','entertainment','health','science','sports','technology']
         return (
             <div>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -12,29 +13,13 @@ export class NavBar extends Component {
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNavDropdown">
                             <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <Link className="nav-link text-capitalize" to="/">general</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link text-capitalize" to="/business">business</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link text-capitalize" to="/entertainment">entertainment</Link>
-                                </li>
-
-                                <li className="nav-item">
-                                    <Link className="nav-link text-capitalize" to="/health">health</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link text-capitalize" to="/science">science</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link text-capitalize" to="/sports">sports</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link text-capitalize" to="/technology">technology</Link>
-                                </li>
-
+                                {
+                                    navItem.map((e)=>{
+                                       return <li className="nav-item">
+                                                 <Link className="nav-link text-capitalize" to={`/${e}`}>{e}</Link>
+                                              </li>
+                                    })
+                                }
                             </ul>
                         </div>
                     </div>
